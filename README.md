7119fb961a8ec551f93114dfcf5c102f7438f691
Weitian Leung <weitianleung@gmail.com>
2017-04-26 00:48:06 +0800

    add setup.py supports
    Now use *setup.py build* instead of those scripts
    build_exe might not work perfectly at the moment

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # gitc
+
 A file conflict viewer for git
 
-It also can be a commit viewer if pass --log-mode/-l argument
-
-**Features**
+## Features
 
 * Two branches view for easy comparing a conflict commit base on the file.
 * Visualize white spaces and carriage return for easy diff compare.
@@ -12,7 +11,20 @@ It also can be a commit viewer if pass --log-mode/-l argument
 * Copy commit summary as HTML format for pasting.
 * Custom pattern for creating links.
 
-**TODO**
+## Build & Run
+
+- Using source directly
+
+You should run build for the first time: *python setup.py build*
+Now run **gitc** which in the source directory.
+
+- Using binary
+
+Run *python setup.py build_exe*, the binary will be generated in the **build** directory.
+NOTE: Run *python setup.py build* before *build_exe*
+
+
+## TODO
 
 1. Improve performance for finding commit, use ==--stdin== to avoid start git process many times e.t.c.
 2. Auto collect git conflict files for merging.
---
+++ cxfreeze-setup.py
@@ -0,0 +1,43 @@
+import sys
+
+from glob import glob
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+excludes=["Tkinter"]
+includes = ["logview", "colorwidget"]
+includeFiles = [("data/icons/gitc.svg", "data/icons/gitc.svg")]
+
+for qm in glob("data/translations/*.qm"):
+    includeFiles.append((qm, qm))
+
+# FIXME: find a better way merge ui files
+for ui in glob("ui/*.py"):
+    includeFiles.append((ui, ui))
+
+buildOptions = dict(
+    packages=[],
+    excludes=excludes,
+    includes=includes,
+    include_files=includeFiles,
+    silent=True)
+
+base = None
+icon = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+    icon = "data/icons/gitc.ico"
+
+executables = [
+    Executable('gitc',
+               base=base,
+               icon=icon
+               )]
+
+setup(name='gitc',
+      version='1.0',
+      description='A file conflict viewer for git',
+      options=dict(build_exe=buildOptions),
+      executables=executables,
+      )
