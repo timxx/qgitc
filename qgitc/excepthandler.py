03eb15982e552cc601d063713da07dead5610db5
Weitian Leung <weitianleung@gmail.com>
2020-04-25 17:14:58 +0800

    Show messagebox only in main thread

--- qgitc/excepthandler.py
+++ qgitc/excepthandler.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from PySide2.QtWidgets import QMessageBox
+from PySide2.QtCore import (
+    QCoreApplication,
+    QThread)
 
 import traceback
 
 
 def ExceptHandler(etype, value, tb):
-    msg = traceback.format_exception(etype, value, tb)
-    QMessageBox.warning(None, "Exception occurred!",
-                        "".join(msg), QMessageBox.Ok)
+    if QCoreApplication.instance().thread() == QThread.currentThread():
+        msg = traceback.format_exception(etype, value, tb)
+        QMessageBox.warning(None, "Exception occurred!",
+                            "".join(msg), QMessageBox.Ok)
+    else:
+        traceback.print_exception(etype, value, tb)
