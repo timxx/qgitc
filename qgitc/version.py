4d899a580facd58df9bcf8ed4bf1a5c1374a7da2
Weitian Leung <weitianleung@gmail.com>
2020-05-14 08:26:56 +0800

    Bump version

--- qgitc/version.py
+++ qgitc/version.py
@@ -2,8 +2,8 @@
 
 
 VERSION_MAJOR = 2
-VERSION_MINOR = 0
-VERSION_PATCH = 2
+VERSION_MINOR = 1
+VERSION_PATCH = 0
 VERSION = "{}.{}.{}".format(VERSION_MAJOR,
                             VERSION_MINOR,
                             VERSION_PATCH)
