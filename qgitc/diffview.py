073b91077c0d6d068a9e7d2f5daf1ac7f4146b51
Weitian Leung <weitianleung@gmail.com>
2020-05-28 08:58:11 +0800

    Cursor -> TextCursor

--- qgitc/diffview.py
+++ qgitc/diffview.py
@@ -15,6 +15,7 @@ from .textline import (
     TextLine,
     SourceTextLineBase)
 from .colorschema import ColorSchema
+from .textcursor import TextCursor
 
 import re
 
@@ -509,79 +510,6 @@ class DiffView(QWidget):
             self.splitter.restoreState(state)
 
 
-class Cursor():
-
-    def __init__(self):
-        self.clear()
-
-    def clear(self):
-        self._beginLine = -1
-        self._beginPos = -1
-        self._endLine = -1
-        self._endPos = -1
-
-    def isValid(self):
-        return self._beginLine != -1 and \
-            self._endLine != -1 and \
-            self._beginPos != -1 and \
-            self._endPos != -1
-
-    def hasMultiLines(self):
-        if not self.isValid():
-            return False
-
-        return self._beginLine != self._endLine
-
-    def hasSelection(self):
-        if not self.isValid():
-            return False
-
-        if self.hasMultiLines():
-            return True
-        return self._beginPos != self._endPos
-
-    def within(self, line):
-        if not self.hasSelection():
-            return False
-
-        if line >= self.beginLine() and line <= self.endLine():
-            return True
-
-        return False
-
-    def beginLine(self):
-        return min(self._beginLine, self._endLine)
-
-    def endLine(self):
-        return max(self._beginLine, self._endLine)
-
-    def beginPos(self):
-        if self._beginLine == self._endLine:
-            return min(self._beginPos, self._endPos)
-        elif self._beginLine < self._endLine:
-            return self._beginPos
-        else:
-            return self._endPos
-
-    def endPos(self):
-        if self._beginLine == self._endLine:
-            return max(self._beginPos, self._endPos)
-        elif self._beginLine < self._endLine:
-            return self._endPos
-        else:
-            return self._beginPos
-
-    def moveTo(self, line, pos):
-        self._beginLine = line
-        self._beginPos = pos
-        self._endLine = line
-        self._endPos = pos
-
-    def selectTo(self, line, pos):
-        self._endLine = line
-        self._endPos = pos
-
-
 class DiffTextLine(SourceTextLineBase):
 
     def __init__(self, viewer, text):
@@ -659,7 +587,7 @@ class PatchViewer(QAbstractScrollArea):
         self.wordPattern = None
         self.findPattern = None
 
-        self.cursor = Cursor()
+        self.cursor = TextCursor()
         self.tripleClickTimer = QElapsedTimer()
         self.clickOnLink = False
         self.currentLink = None
