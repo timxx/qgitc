9502a4f2db6d0388ef1c7f88fce1ab3f70dc8300
Weitian Leung <weitianleung@gmail.com>
2020-05-28 08:58:11 +0800

    Cursor -> TextCursor

---
+++ qgitc/textcursor.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+
+__all__ = ["TextCursor"]
+
+
+class TextCursor():
+
+    def __init__(self):
+        self.clear()
+
+    def clear(self):
+        self._beginLine = -1
+        self._beginPos = -1
+        self._endLine = -1
+        self._endPos = -1
+
+    def isValid(self):
+        return self._beginLine != -1 and \
+            self._endLine != -1 and \
+            self._beginPos != -1 and \
+            self._endPos != -1
+
+    def hasMultiLines(self):
+        if not self.isValid():
+            return False
+
+        return self._beginLine != self._endLine
+
+    def hasSelection(self):
+        if not self.isValid():
+            return False
+
+        if self.hasMultiLines():
+            return True
+        return self._beginPos != self._endPos
+
+    def within(self, line):
+        if not self.hasSelection():
+            return False
+
+        if line >= self.beginLine() and line <= self.endLine():
+            return True
+
+        return False
+
+    def beginLine(self):
+        return min(self._beginLine, self._endLine)
+
+    def endLine(self):
+        return max(self._beginLine, self._endLine)
+
+    def beginPos(self):
+        if self._beginLine == self._endLine:
+            return min(self._beginPos, self._endPos)
+        elif self._beginLine < self._endLine:
+            return self._beginPos
+        else:
+            return self._endPos
+
+    def endPos(self):
+        if self._beginLine == self._endLine:
+            return max(self._beginPos, self._endPos)
+        elif self._beginLine < self._endLine:
+            return self._endPos
+        else:
+            return self._beginPos
+
+    def moveTo(self, line, pos):
+        self._beginLine = line
+        self._beginPos = pos
+        self._endLine = line
+        self._endPos = pos
+
+    def selectTo(self, line, pos):
+        self._endLine = line
+        self._endPos = pos
