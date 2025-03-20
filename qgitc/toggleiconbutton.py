8cd30eec0f9bd3450906ecbb84ffe4000c06e58b
Weitian Leung <weitianleung@gmail.com>
2025-03-20 08:19:00 +0800

    Fix find widget tool icon for dark theme

--- qgitc/toggleiconbutton.py
+++ qgitc/toggleiconbutton.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from PySide6.QtWidgets import QToolButton
-from PySide6.QtGui import (QPainter, QIcon, QPen)
+from PySide6.QtGui import (QPainter, QIcon, QPen, QPalette)
 from PySide6.QtCore import (QRect, QPoint, Qt)
 
 
@@ -13,13 +13,19 @@ class ToggleIconButton(QToolButton):
         self.setIcon(icon)
         self.setCheckable(True)
         self.setCursor(Qt.PointingHandCursor)
+        self._isColoredIcon = True
+
+    def setColoredIcon(self, isColoredIcon):
+        self._isColoredIcon = isColoredIcon
+        self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
+        palette = self.palette()
 
         if self.isChecked():
             oldPen = painter.pen()
-            painter.setPen(QPen(Qt.black, 1))
+            painter.setPen(QPen(palette.windowText().color(), 1))
             rc = self.rect().adjusted(1, 1, -1, -1)
             painter.drawRect(rc)
             painter.setPen(oldPen)
@@ -30,4 +36,17 @@ class ToggleIconButton(QToolButton):
         pixmap = self.icon().pixmap(self._iconSize, self.devicePixelRatio(), state, QIcon.Off)
         pixmapRect = QRect(QPoint(0, 0), self._iconSize)
         pixmapRect.moveCenter(self.rect().center())
+
+        if self._isColoredIcon and qApp.isDarkTheme():
+            p = QPainter(pixmap)
+            p.setPen(Qt.NoPen)
+            group = QPalette.Disabled
+            if self.isEnabled():
+                group = QPalette.Active if self.isDown() else QPalette.Normal
+
+            brush = palette.brush(group, QPalette.WindowText)
+            p.setCompositionMode(QPainter.CompositionMode_SourceIn)
+            p.fillRect(pixmap.rect(), brush)
+            p.end()
+
         painter.drawPixmap(pixmapRect, pixmap)
