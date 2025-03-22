c8ac91bb244637a2c63c1274332b3f012edadc89
Weitian Leung <weitianleung@gmail.com>
2025-03-23 00:08:26 +0800

    Now implement line number display in text viewer basic class

--- qgitc/blameview.py
+++ qgitc/blameview.py
@@ -206,16 +206,11 @@ class RevisionPanel(TextViewer):
         self.updateFont(qApp.settings().diffViewFont())
 
         fm = QFontMetrics(self._font)
-        self._sha1Width = fm.horizontalAdvance('a') * ABBREV_N
-        self._dateWidth = fm.horizontalAdvance("2020-05-27")
         self._space = fm.horizontalAdvance(' ')
-        self._digitWidth = fm.horizontalAdvance('9')
-        self._maxNameWidth = 12 * fm.horizontalAdvance('W')
 
-        width = self._sha1Width + self._space * 6
-        width += self._dateWidth
-        width += self._maxNameWidth
-        width += self._digitWidth * 6
+        sampleText = "a" * ABBREV_N + " 2020-05-27 Long Author Name"
+        width = fm.horizontalAdvance(sampleText)
+        width += self._space * 2
         self.resize(width, self._viewer.height())
 
     def appendRevisions(self, revs):
@@ -348,8 +343,7 @@ class RevisionPanel(TextViewer):
         colorSchema = qApp.colorSchema()
 
         textLineCount = self.textLineCount()
-        digitCount = max(3, len(str(textLineCount)))
-        x = width - digitCount * self._digitWidth - self._space * 2
+        x = width - self._space
         oldPen = painter.pen()
         painter.setPen(colorSchema.Splitter)
         painter.drawLine(x, y, x, self.height())
@@ -361,7 +355,6 @@ class RevisionPanel(TextViewer):
             return
 
         startLine = self.firstVisibleLine()
-        ascent = QFontMetrics(self._font).ascent()
 
         for i in range(startLine, textLineCount):
             line = self.textLineAt(i)
@@ -374,12 +367,6 @@ class RevisionPanel(TextViewer):
             line.draw(painter, QPointF(self._space, y))
 
             painter.restore()
-
-            lineNumber = str(i + 1)
-            x = width - len(lineNumber) * self._digitWidth - self._space
-            painter.setPen(colorSchema.LineNumber)
-            painter.drawText(x, y + ascent, lineNumber)
-            painter.setPen(oldPen)
 
             y += self._viewer.lineHeight
             if y > self.height():
@@ -419,6 +406,7 @@ class BlameSourceViewer(SourceViewer):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setPanel(RevisionPanel(self))
+        self.setShowLineNumbers(True)
 
         self._panel.revisionActivated.connect(
             self.revisionActivated)
