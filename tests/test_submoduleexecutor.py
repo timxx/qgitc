756ab1f0c75829974522b82bd5ee58ab2f5d3498
Weitian Leung <weitianleung@gmail.com>
2025-04-26 17:00:35 +0800

    Add basic unit tests for `SubmoduleExecutor`
    This includes tests for submission, cancellation, and abort actions to ensure the proper behavior of the executor.

---
+++ tests/test_submoduleexecutor.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import time
+from unittest.mock import patch
+from PySide6.QtTest import QSignalSpy, QTest
+from qgitc.cancelevent import CancelEvent
+from qgitc.submoduleexecutor import SubmoduleExecutor
+from tests.base import TestBase
+
+
+def _dummyAction(submodule, data, cancelEvent: CancelEvent):
+    raise Exception("Should not be called")
+
+
+def _dummyAction2(submodule, data, cancelEvent: CancelEvent):
+    return "Hello"
+
+
+def _dummyResult(*kargs):
+    raise Exception("Should not be called")
+
+
+class TestSubmoduleExecutor(TestBase):
+    def testSubmit(self):
+        executor = SubmoduleExecutor()
+        spy = QSignalSpy(executor.finished)
+
+        with patch("test_submoduleexecutor._dummyAction") as dummy:
+            dummy.return_value = "Hello"
+            executor.submit(None, _dummyAction)
+            QTest.qWait(100)
+            self.assertTrue(spy.wait(50))
+            self.assertTrue(spy.count(), 1)
+            dummy.assert_called_once()
+
+        with patch("test_submoduleexecutor._dummyResult") as dummy:
+            executor.submit([None], _dummyAction2, _dummyResult)
+            QTest.qWait(100)
+            self.assertTrue(spy.wait(50))
+            self.assertTrue(spy.count(), 1)
+            dummy.assert_called_once_with("Hello")
+
+    def _cancelAction(self, submodule, data, cancelEvent: CancelEvent):
+        time.sleep(0.1)
+        if cancelEvent.isSet():
+            return True
+        return False
+
+    def testCancel(self):
+        executor = SubmoduleExecutor()
+        spy = QSignalSpy(executor.finished)
+
+        with patch("test_submoduleexecutor._dummyResult") as dummy:
+            executor.submit(None, self._cancelAction, _dummyResult)
+            QTest.qWait(50)
+            self.assertTrue(executor.isRunning())
+            executor.cancel()
+            QTest.qWait(600)
+            self.assertFalse(executor.isRunning())
+            # the signal is disconnected
+            self.assertFalse(spy.wait(500))
+            dummy.assert_not_called()
+
+    def _blockAction(self, submodule, data, cancelEvent: CancelEvent):
+        time.sleep(10)
+
+    def testAbort(self):
+        executor = SubmoduleExecutor()
+
+        with patch("logging.Logger.warning") as warning:
+            executor.submit(None, self._blockAction)
+            QTest.qWait(100)
+            self.assertTrue(executor.isRunning())
+            executor.cancel()
+
+            # executor internal waits for 500ms
+            QTest.qWait(600)
+            self.assertFalse(executor.isRunning())
+
+            warning.assert_called_once_with(
+                "Terminating submodule thread (%s)", "_blockAction")
