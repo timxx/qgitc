aa5296eb1ea9beadd1863b65fa189d375d0ffec6
Weitian Leung <weitianleung@gmail.com>
2025-04-27 17:49:38 +0800

    Bump patch version to 1 for upcoming release

--- qgitc/version.py
+++ qgitc/version.py
@@ -3,7 +3,7 @@
 
 VERSION_MAJOR = 5
 VERSION_MINOR = 4
-VERSION_PATCH = 0
+VERSION_PATCH = 1
 VERSION = "{}.{}.{}".format(VERSION_MAJOR,
                             VERSION_MINOR,
                             VERSION_PATCH)
