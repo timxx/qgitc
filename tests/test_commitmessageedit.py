725aa36297e1419e964336a445df4522f5a76f3e
Weitian Leung <weitianleung@gmail.com>
2025-05-01 15:46:54 +0800

    Add tests for tab group functionality in CommitMessageEdit

--- tests/test_commitmessageedit.py
+++ tests/test_commitmessageedit.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from PySide6.QtCore import Qt
 from PySide6.QtGui import QTextCursor
+from PySide6.QtTest import QTest
 from qgitc.commitmessageedit import CommitMessageEdit
 from tests.base import TestBase
 
@@ -72,3 +74,135 @@ class TestCommitMessageEdit(TestBase):
 
                     fg = self.getForegroundColor(cursor).color()
                     self.assertEqual(fg, inlineSpanColor)
+
+    def testTabGroups(self):
+        # we enable by default
+        self.assertTrue(self.app.settings().tabToNextGroup())
+        groupChars = [groupChar.strip() for groupChar in self.app.settings(
+        ).groupChars().split(" ") if len(groupChar.strip()) == 2]
+        self.assertEqual(2, len(groupChars))
+        self.assertEqual(groupChars[0], "【】")
+        self.assertEqual(groupChars[1], "[]")
+
+        # do nothing for empty document
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertTrue(self.edit.document().isEmpty())
+
+        self.edit.setPlainText("No groups")
+
+        # should not hangs
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 9)
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 9)
+
+        QTest.keyClick(self.edit, Qt.Key_Backtab)
+        self.assertEqual(self.edit.textCursor().position(), 0)
+
+        self.edit.setPlainText("Line1\n\nLine2")
+        cursor = self.edit.textCursor()
+        cursor.setPosition(6)
+        self.edit.setTextCursor(cursor)
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 12)
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 12)
+
+        QTest.keyClick(self.edit, Qt.Key_Backtab)
+        self.assertEqual(self.edit.textCursor().position(), 0)
+
+        self.edit.setPlainText("【hello")
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 6)
+
+        QTest.keyClick(self.edit, Qt.Key_Backtab)
+        self.assertEqual(self.edit.textCursor().position(), 0)
+
+        self.edit.setPlainText("【hello】")
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 6)
+
+        QTest.keyClick(self.edit, Qt.Key_Backtab)
+        self.assertEqual(self.edit.textCursor().position(), 0)
+
+        self.edit.setPlainText("[hello]\n[world]")
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 6)
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 14)
+
+        # next round
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 6)
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 14)
+
+        QTest.keyClick(self.edit, Qt.Key_Backtab)
+        self.assertEqual(self.edit.textCursor().position(), 6)
+
+        QTest.keyClick(self.edit, Qt.Key_Backtab)
+        self.assertEqual(self.edit.textCursor().position(), 14)
+
+        # break lines
+        self.edit.setPlainText("[hello\n]")
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 7)
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 7)
+
+        cursor.movePosition(QTextCursor.MoveOperation.End)
+        self.edit.setTextCursor(cursor)
+
+        QTest.keyClick(self.edit, Qt.Key_Backtab)
+        self.assertEqual(self.edit.textCursor().position(), 0)
+
+        QTest.keyClick(self.edit, Qt.Key_Backtab)
+        self.assertEqual(self.edit.textCursor().position(), 0)
+
+        # we enable by default
+        self.assertTrue(self.app.settings().ignoreCommentLine())
+
+        self.edit.setPlainText("# [hello]")
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual(self.edit.textCursor().position(), 9)
+
+        QTest.keyClick(self.edit, Qt.Key_Backtab)
+        self.assertEqual(self.edit.textCursor().position(), 0)
+
+    def testNoGroups(self):
+        settings = self.app.settings()
+        groupChars = settings.groupChars()
+        settings.setGroupChars("")
+
+        self.edit.setPlainText("[hello]")
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+
+        self.assertEqual(0, self.edit.textCursor().position())
+
+        settings.setGroupChars(groupChars)
+
+    def testDisableTabGroups(self):
+        settings = self.app.settings()
+        oldValue = settings.tabToNextGroup()
+
+        settings.setTabToNextGroup(False)
+
+        QTest.keyClick(self.edit, Qt.Key_Tab)
+        self.assertEqual("\t", self.edit.toPlainText())
+
+        settings.setTabToNextGroup(oldValue)
+
+    def testNormalKey(self):
+        QTest.keyClick(self.edit, Qt.Key_0)
+        self.assertEqual("0", self.edit.toPlainText())
