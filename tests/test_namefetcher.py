513a9e51879702dc5123407ce29267aa2eec6620
Weitian Leung <weitianleung@gmail.com>
2025-05-28 12:49:05 +0800

    Add unit tests for NameFetcher to validate file renaming

---
+++ tests/test_namefetcher.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from qgitc.gitutils import Git
+from qgitc.namefetcher import NameFetcher
+from tests.base import TestBase
+
+
+class TestNameFetcher(TestBase):
+
+    def doCreateRepo(self):
+        super().doCreateRepo()
+
+        Git.checkOutput(["mv", "test.py", "foo.py"])
+        Git.commit("Renamed test.py to foo.py")
+
+    def testFetch(self):
+        sha1Names = {}
+
+        def _updateNames(data):
+            for sha1, name in data:
+                sha1Names[sha1] = name
+
+        fetcher = NameFetcher()
+        fetcher.dataAvailable.connect(_updateNames)
+        fetcher.fetch("foo.py")
+        self.wait(1000, lambda: fetcher._process is not None)
+
+        self.assertEqual(len(sha1Names), 2)
+
+        names = set(sha1Names.values())
+        self.assertSetEqual(names, {"test.py", "foo.py"})
