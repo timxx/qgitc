cb744784a130fa9c0acfefc202e34365753630ad
Weitian Leung <weitianleung@gmail.com>
2025-07-14 20:30:30 +0800

    Prepare for using pygit2 to fetch status

--- qgitc/statusfetcher.py
+++ qgitc/statusfetcher.py
@@ -10,6 +10,53 @@ from qgitc.gitutils import Git
 from qgitc.submoduleexecutor import SubmoduleExecutor
 
 
+def _fetchStatusGit(submodule, cancelEvent: CancelEvent, showUntrackedFiles=True, showIgnoredFiles=False):
+    repoDir = fullRepoDir(submodule)
+    try:
+        data = Git.status(repoDir, showUntrackedFiles, showIgnoredFiles)
+        if not data:
+            return None, None
+    except Exception:
+        logger.exception("Error fetching status for `%s`", repoDir)
+        return None, None
+
+    if cancelEvent.isSet():
+        logger.debug(
+            "Cancel event set, aborting status fetch for `%s`", repoDir)
+        return None, None
+
+    lines = data.rstrip(b'\0').split(b'\0')
+    result = []
+    i = 0
+    while i < len(lines):
+        line = lines[i]
+        i += 1
+
+        assert (len(line) > 3)
+        if line[2] != 32:
+            logger.warning("Unexpected status line `%s`", line.decode())
+            continue
+
+        status = line[:2].decode()
+        file = line[3:].decode()
+        repoFile = os.path.normpath(os.path.join(
+            submodule, file) if submodule and submodule != '.' else file)
+
+        oldRepoFile = None
+        # rename followed by old name
+        if status[0] == 'R' and i + 1 < len(lines):
+            oldFile = lines[i].decode()
+            oldRepoFile = os.path.normpath(os.path.join(
+                submodule, oldFile) if submodule and submodule != '.' else oldFile)
+            i += 1
+
+        result.append((status, repoFile, oldRepoFile))
+
+    logger.debug("Status fetch result for `%s`: %s", repoDir, result)
+
+    return submodule, result
+
+
 class StatusFetcher(SubmoduleExecutor):
     resultAvailable = Signal(str, list)
     branchInfoAvailable = Signal(str, str)
@@ -63,47 +110,5 @@ class StatusFetcher(SubmoduleExecutor):
             self.resultAvailable.emit(submodule, result)
 
     def _fetchStatus(self, submodule, userData, cancelEvent: CancelEvent):
-        repoDir = fullRepoDir(submodule)
-        try:
-            data = Git.status(
-                repoDir, self._showUntrackedFiles, self._showIgnoredFiles)
-            if not data:
-                return None, None
-        except Exception:
-            logger.exception("Error fetching status for `%s`", repoDir)
-            return None, None
-
-        if cancelEvent.isSet():
-            logger.debug("Cancel event set, aborting status fetch for `%s`", repoDir)
-            return None, None
-
-        lines = data.rstrip(b'\0').split(b'\0')
-        result = []
-        i = 0
-        while i < len(lines):
-            line = lines[i]
-            i += 1
-
-            assert (len(line) > 3)
-            if line[2] != 32:
-                logger.warning("Unexpected status line `%s`", line.decode())
-                continue
-
-            status = line[:2].decode()
-            file = line[3:].decode()
-            repoFile = os.path.normpath(os.path.join(
-                submodule, file) if submodule and submodule != '.' else file)
-
-            oldRepoFile = None
-            # rename followed by old name
-            if status[0] == 'R' and i + 1 < len(lines):
-                oldFile = lines[i].decode()
-                oldRepoFile = os.path.normpath(os.path.join(
-                    submodule, oldFile) if submodule and submodule != '.' else oldFile)
-                i += 1
-
-            result.append((status, repoFile, oldRepoFile))
-
-        logger.debug("Status fetch result for `%s`: %s", repoDir, result)
-
-        return submodule, result
+        return _fetchStatusGit(
+            submodule, cancelEvent, self._showUntrackedFiles, self._showIgnoredFiles)
