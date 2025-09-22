3542689281d92e15cbc9a29b88bc269e7bbcb1a1
Weitian Leung <weitianleung@gmail.com>
2025-09-22 21:14:59 +0800

    Implement branch loading
    To reuse the existing repository loading logic, the log window is now required to switch repositories.

--- qgitc/branchcomparewindow.py
+++ qgitc/branchcomparewindow.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+
+from qgitc.applicationbase import ApplicationBase
+from qgitc.events import ShowCommitEvent
+from qgitc.gitutils import Git
 from qgitc.statewindow import StateWindow
 from qgitc.ui_branchcomparewindow import Ui_BranchCompareWindow
 
@@ -18,3 +22,72 @@ class BranchCompareWindow(StateWindow):
         height = self.ui.splitter.sizeHint().height()
         sizes = [height * 4 / 5, height * 1 / 5]
         self.ui.splitter.setSizes(sizes)
+
+        self._isFirstShow = True
+        self._setupSignals()
+
+    def _setupSignals(self):
+        # TODO: delayed loading
+        self.ui.cbBaseBranch.currentIndexChanged.connect(self._loadChanges)
+        self.ui.cbTargetBranch.currentIndexChanged.connect(self._loadChanges)
+        self.ui.btnShowLogWindow.clicked.connect(self._showLogWindow)
+
+    def showEvent(self, event):
+        super().showEvent(event)
+
+        if not self._isFirstShow:
+            return
+
+        self._reloadBranches()
+
+        self._isFirstShow = False
+
+    def _reloadBranches(self):
+        branches = Git.branches()
+
+        curBranchIdx = -1
+        defBranchIdx = -1
+        self.ui.cbBaseBranch.blockSignals(True)
+        self.ui.cbTargetBranch.blockSignals(True)
+
+        self.ui.cbBaseBranch.clear()
+        self.ui.cbTargetBranch.clear()
+
+        for branch in branches:
+            branch = branch.strip()
+            if branch.startswith("remotes/origin/"):
+                if not branch.startswith("remotes/origin/HEAD"):
+                    self.ui.cbBaseBranch.addItem(branch)
+                    self.ui.cbTargetBranch.addItem(branch)
+            elif branch:
+                if branch.startswith("*"):
+                    pure_branch = branch.replace("*", "").strip()
+                    self.ui.cbBaseBranch.addItem(pure_branch)
+                    self.ui.cbTargetBranch.addItem(pure_branch)
+                    defBranchIdx = self.ui.cbBaseBranch.count() - 1
+                    if curBranchIdx == -1:
+                        curBranchIdx = defBranchIdx
+                else:
+                    if branch.startswith("+ "):
+                        branch = branch[2:]
+                    branch = branch.strip()
+                    self.ui.cbBaseBranch.addItem(branch)
+                    self.ui.cbTargetBranch.addItem(branch)
+
+        if curBranchIdx == -1:
+            curBranchIdx = defBranchIdx
+        if curBranchIdx != -1:
+            self.ui.cbBaseBranch.setCurrentIndex(curBranchIdx)
+
+        self.ui.cbTargetBranch.setCurrentIndex(-1)
+        self.ui.cbBaseBranch.blockSignals(False)
+        self.ui.cbTargetBranch.blockSignals(False)
+
+        self._loadChanges()
+
+    def _loadChanges(self):
+        pass
+
+    def _showLogWindow(self):
+        app = ApplicationBase.instance()
+        app.postEvent(app, ShowCommitEvent(None))
