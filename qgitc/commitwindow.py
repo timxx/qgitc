d3147caadacb95374bcc5439a761e1d4221c9a1a
Weitian Leung <weitianleung@gmail.com>
2025-09-23 16:52:21 +0800

    Move StatusFileListModel and related classes to filestatus.py
    Extract file status model and info classes from commitwindow.py into a dedicated filestatus.py module for better separation and reuse.

--- qgitc/commitwindow.py
+++ qgitc/commitwindow.py
@@ -49,8 +49,9 @@ from qgitc.coloredlabel import ColoredLabel
 from qgitc.commitactiontablemodel import ActionCondition, CommitAction
 from qgitc.common import dataDirPath, fullRepoDir, logger, toSubmodulePath
 from qgitc.difffetcher import DiffFetcher
-from qgitc.diffview import DiffView, _makeTextIcon
+from qgitc.diffview import DiffView
 from qgitc.events import CodeReviewEvent, LocalChangesCommittedEvent, ShowCommitEvent
+from qgitc.filestatus import StatusFileListModel
 from qgitc.findconstants import FindFlags
 from qgitc.gitutils import Git
 from qgitc.ntpdatetime import getNtpDateTime
@@ -67,117 +68,6 @@ class FileStatus(Enum):
     Untracked = 0
     Unstaged = 1
     Staged = 2
-
-
-class StatusFileInfo():
-
-    def __init__(self, file: str, repoDir: str, statusCode: str, oldFile: str = None):
-        self.file = file
-        self.repoDir = repoDir
-        self.statusCode = statusCode
-        self.oldFile = oldFile
-
-
-class StatusFileListModel(QAbstractListModel):
-
-    StatusCodeRole = Qt.UserRole
-    RepoDirRole = Qt.UserRole + 1
-    OldFileRole = Qt.UserRole + 2
-
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self._fileList: List[StatusFileInfo] = []
-        self._icons = {}
-
-    def rowCount(self, parent=QModelIndex()):
-        if parent.isValid():
-            return 0
-
-        return len(self._fileList)
-
-    def flags(self, index):
-        return Qt.ItemIsEnabled | Qt.ItemIsSelectable
-
-    def removeRows(self, row, count, parent=QModelIndex()):
-        if row < 0 or (row + count) > self.rowCount(parent) or count < 1:
-            return False
-
-        self.beginRemoveRows(QModelIndex(), row, row + count - 1)
-        del self._fileList[row: row + count]
-        self.endRemoveRows()
-
-        return True
-
-    def data(self, index, role=Qt.DisplayRole):
-        row = index.row()
-        if row < 0 or row >= self.rowCount():
-            return False
-
-        if role == Qt.DisplayRole:
-            return self._fileList[row].file
-        elif role == StatusFileListModel.StatusCodeRole:
-            return self._fileList[row].statusCode
-        elif role == StatusFileListModel.RepoDirRole:
-            return self._fileList[row].repoDir
-        elif role == Qt.DecorationRole:
-            return self._statusIcon(self._fileList[row].statusCode)
-        elif role == Qt.ToolTipRole:
-            oldFile = self._fileList[row].oldFile
-            if oldFile:
-                return self.tr("Renamed from: ") + oldFile
-        elif role == StatusFileListModel.OldFileRole:
-            return self._fileList[row].oldFile
-
-        return None
-
-    def addFile(self, file: str, repoDir: str, statusCode: str, oldFile: str = None):
-        rowCount = self.rowCount()
-        self.beginInsertRows(QModelIndex(), rowCount, rowCount)
-        self._fileList.append(StatusFileInfo(
-            file, repoDir, statusCode, oldFile))
-        self.endInsertRows()
-
-    def removeFile(self, file: str, repoDir: str):
-        if not self._fileList:
-            return None
-
-        for i, fileInfo in enumerate(self._fileList):
-            if fileInfo.file == file and fileInfo.repoDir == repoDir:
-                break
-        else:
-            return None
-
-        self.beginRemoveRows(QModelIndex(), i, i)
-        info = self._fileList.pop(i)
-        self.endRemoveRows()
-        return info
-
-    def clear(self):
-        self.removeRows(0, self.rowCount())
-
-    def _statusIcon(self, statusCode):
-        icon = self._icons.get(statusCode)
-        if not icon:
-            font: QFont = ApplicationBase.instance().font()
-            font.setBold(True)
-            if statusCode == "A":
-                color = ApplicationBase.instance().colorSchema().Adding
-            elif statusCode == "M":
-                color = ApplicationBase.instance().colorSchema().Modified
-            elif statusCode == "D":
-                color = ApplicationBase.instance().colorSchema().Deletion
-            elif statusCode == "R":
-                color = ApplicationBase.instance().colorSchema().Renamed
-            elif statusCode == "?":
-                color = ApplicationBase.instance().colorSchema().Untracked
-            elif statusCode == "!":
-                color = ApplicationBase.instance().colorSchema().Ignored
-            else:
-                color = ApplicationBase.instance().palette().windowText().color()
-            icon = _makeTextIcon(statusCode, color, font)
-            self._icons[statusCode] = icon
-        return icon
-
 
 class GitErrorEvent(QEvent):
     Type = QEvent.User + 2
