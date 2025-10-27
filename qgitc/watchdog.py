250ad41bb8d31412d150ecc2aa37a7e752ee50b7
Weitian Leung <weitianleung@gmail.com>
2025-10-27 14:43:27 +0800

    Fix potential `None` exception
    Since `WatchdogWorker` starts in a child thread and stops on the main thread,
    if we stop before it starts running, we may access a `None` _timer.

--- qgitc/watchdog.py
+++ qgitc/watchdog.py
@@ -26,18 +26,26 @@ class WatchdogWorker(QObject):
         self._isMainThreadAlive = True
         self._timer: QTimer = None
         self._report = False
+        self._lock = threading.Lock()
+        self._requestStop = False
 
     def start(self):
-        self._timer = QTimer(self)
-        self._timer.setInterval(6000)
-        self._timer.timeout.connect(self._checkMainThreadAlive)
-        self._timer.start()
+        with self._lock:
+            if self._requestStop:
+                return
+            self._requestStop = False
+            self._timer = QTimer(self)
+            self._timer.setInterval(6000)
+            self._timer.timeout.connect(self._checkMainThreadAlive)
+            self._timer.start()
 
     def stop(self):
-        if not self._timer:
-            return
-        self._timer.deleteLater()
-        self._timer = None
+        with self._lock:
+            self._requestStop = True
+            if not self._timer:
+                return
+            self._timer.deleteLater()
+            self._timer = None
 
     def notifyMainThreadAlive(self):
         self._isMainThreadAlive = True
